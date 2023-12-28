88468278c20114395f7ed13de73ab60516cf0127
Yanshuf <sefiuzan812@gmail.com>
2023-12-29 01:50:41 +0200

    pinecode is fixed

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -8,41 +8,41 @@ import {
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
 import { PineconeStore } from 'langchain/vectorstores/pinecone'
-import { getPineconeClient } from '@/lib/pinecone'
-import { getUserSubscriptionPlan } from '@/lib/stripe'
-import { PLANS } from '@/config/stripe'
+import { getUserSubscriptionPlan } from "@/lib/stripe";
+import { PLANS } from "@/config/stripe";
+import { Pinecone } from '@pinecone-database/pinecone'
 
-const f = createUploadthing()
+const f = createUploadthing();
 
 const middleware = async () => {
-  const { getUser } = getKindeServerSession()
-  const user = getUser()
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
 
-  if (!user || !user.id) throw new Error('Unauthorized')
+  if (!user || !user.id) throw new Error("Unauthorized");
 
-  const subscriptionPlan = await getUserSubscriptionPlan()
+  const subscriptionPlan = await getUserSubscriptionPlan();
 
-  return { subscriptionPlan, userId: user.id }
-}
+  return { subscriptionPlan, userId: user.id };
+};
 
 const onUploadComplete = async ({
   metadata,
   file,
 }: {
-  metadata: Awaited<ReturnType<typeof middleware>>
+  metadata: Awaited<ReturnType<typeof middleware>>;
   file: {
-    key: string
-    name: string
-    url: string
-  }
+    key: string;
+    name: string;
+    url: string;
+  };
 }) => {
   const isFileExist = await db.file.findFirst({
     where: {
       key: file.key,
     },
-  })
+  });
 
-  if (isFileExist) return
+  if (isFileExist) return;
 
   const createdFile = await db.file.create({
     data: {
@@ -50,84 +50,74 @@ const onUploadComplete = async ({
       name: file.name,
       userId: metadata.userId,
       url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-      uploadStatus: 'PROCESSING',
+      uploadStatus: "PROCESSING",
     },
-  })
+  });
 
   try {
     const response = await fetch(
       `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
-    )
+    );
 
-    const blob = await response.blob()
+    const blob = await response.blob();
 
-    const loader = new PDFLoader(blob)
+    const loader = new PDFLoader(blob);
 
-    const pageLevelDocs = await loader.load()
+    const pageLevelDocs = await loader.load();
 
-    const pagesAmt = pageLevelDocs.length
+    const pagesAmt = pageLevelDocs.length;
 
-    const { subscriptionPlan } = metadata
-    const { isSubscribed } = subscriptionPlan
+    const { subscriptionPlan } = metadata;
+    const { isSubscribed } = subscriptionPlan;
 
     const isProExceeded =
-      pagesAmt >
-      PLANS.find((plan) => plan.name === 'Pro')!.pagesPerPdf
+      pagesAmt > PLANS.find((plan) => plan.name === "Pro")!.pagesPerPdf;
     const isFreeExceeded =
-      pagesAmt >
-      PLANS.find((plan) => plan.name === 'Free')!
-        .pagesPerPdf
+      pagesAmt > PLANS.find((plan) => plan.name === "Free")!.pagesPerPdf;
 
-    if (
-      (isSubscribed && isProExceeded) ||
-      (!isSubscribed && isFreeExceeded)
-    ) {
+    if ((isSubscribed && isProExceeded) || (!isSubscribed && isFreeExceeded)) {
       await db.file.update({
         data: {
-          uploadStatus: 'FAILED',
+          uploadStatus: "PENDING",
         },
         where: {
           id: createdFile.id,
         },
-      })
+      });
     }
 
     // vectorize and index entire document
-    const pinecone = await getPineconeClient()
-    const pineconeIndex = pinecone.Index("yanshuf-ai");
+    const pinecone = new Pinecone()
+    const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX!);
 
     const embeddings = new OpenAIEmbeddings({
       openAIApiKey: process.env.OPENAI_API_KEY,
-    })
+    });
 
-    await PineconeStore.fromDocuments(
-      pageLevelDocs,
-      embeddings,
-      {
-        pineconeIndex,
-        namespace: createdFile.id,
-      }
-    )
+    await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+      pineconeIndex,
+      namespace: createdFile.id,
+    });
 
     await db.file.update({
       data: {
-        uploadStatus: 'SUCCESS',
+        uploadStatus: "SUCCESS",
       },
       where: {
         id: createdFile.id,
       },
-    })
+    });
   } catch (err) {
     await db.file.update({
       data: {
-        uploadStatus: 'FAILED',
+        uploadStatus: "FAILED",
       },
       where: {
         id: createdFile.id,
       },
-    })
+    });
   }
-}
+};
 
 export const ourFileRouter = {
   freePlanUploader: f({ pdf: { maxFileSize: '4MB' } })
--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -31,25 +31,23 @@ const ChatWrapper = ({
             : 500,
       }
     )
-
-  if (isLoading)
-    return (
-      <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
-        <div className='flex-1 flex justify-center items-center flex-col mb-28'>
-          <div className='flex flex-col items-center gap-2'>
-            <Loader2 className='h-8 w-8 text-blue-500 animate-spin' />
-            <h3 className='font-semibold text-xl'>
-              Loading...
-            </h3>
-            <p className='text-zinc-500 text-sm'>
-              We&apos;re preparing your PDF.
-            </p>
-          </div>
+console.log(data?.status);
+if (!data?.status)
+  return (
+    <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+      <div className="flex-1 flex justify-center items-center flex-col mb-28">
+        <div className="flex flex-col items-center gap-2">
+          <Loader2 className="h-8 w-8 text-blue-500 animate-spin" />
+          <h3 className="font-semibold text-xl">Loading...</h3>
+          <p className="text-zinc-500 text-sm">
+            We&apos;re preparing your PDF.
+          </p>
         </div>
-
-        <ChatInput isDisabled />
       </div>
-    )
+
+      <ChatInput isDisabled />
+    </div>
+  );
 
   if (data?.status === 'PROCESSING')
     return (
