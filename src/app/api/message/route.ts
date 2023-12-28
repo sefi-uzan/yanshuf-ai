082012f8dbe630f5051b1ee163277bc251aa994a
Yanshuf <sefiuzan812@gmail.com>
2023-12-28 23:05:14 +0200

    another pinecone

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -50,7 +50,7 @@ export const POST = async (req: NextRequest) => {
   })
 
   const pinecone = await getPineconeClient()
-  const pineconeIndex = pinecone.Index('quill')
+  const pineconeIndex = pinecone.Index("yanshuf-ai");
 
   const vectorStore = await PineconeStore.fromExistingIndex(
     embeddings,
