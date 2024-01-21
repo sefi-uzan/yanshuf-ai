3db06c612081b674d1490f9525a88b9b9e47501f
Yanshuf <sefiuzan812@gmail.com>
2024-01-21 21:01:17 +0200

    more testing including user menu i think

--- tests/e2e/pages/home/navbar.ts
+++ tests/e2e/pages/home/navbar.ts
@@ -1,5 +1,6 @@
 import { Locator, Page } from "@playwright/test";
 import { MobileMenu } from "./mobile-menu";
+import { UserMenu } from "./user-menu";
 
 export class Navbar {
   private readonly navbarLocator: Locator;
@@ -15,6 +16,9 @@ export class Navbar {
   public readonly mobileThemeToggle: Locator;
   public readonly mobileMenuItems: MobileMenu;
 
+  public readonly userMenuItems: UserMenu;
+  public readonly userMenu: Locator;
+
   constructor(page: Page) {
     this.navbarLocator = page.locator("nav > div > div");
     this.navbarTitle = this.navbarLocator.getByRole("link", {
@@ -22,9 +26,7 @@ export class Navbar {
     });
     this.navbarItems = this.navbarLocator.locator("div").nth(1);
     this.mobileNavbarItems = this.navbarLocator.locator("div").nth(0);
-    this.themeToggle = this.navbarItems.locator(
-      "button > span:has-text('Toggle theme')"
-    );
+    this.themeToggle = page.getByTestId("mode-toggle").nth(1);
     this.mobileThemeToggle = this.mobileNavbarItems.locator(
       "button > span:has-text('Toggle theme')"
     );
@@ -32,6 +34,8 @@ export class Navbar {
     this.signInLink = this.navbarItems.locator('a:has-text("Sign in")');
     this.getStartedLink = this.navbarItems.locator('a:has-text("Get started")');
     this.mobileMenu = this.mobileNavbarItems.locator("> button").nth(1);
+    this.userMenu = this.mobileNavbarItems.locator("> button").nth(1);
     this.mobileMenuItems = new MobileMenu(page.getByRole("menu"));
+    this.userMenuItems = new UserMenu(page.getByRole("menu"));
   }
 }
--- tests/e2e/specs/navbar.spec.ts
+++ tests/e2e/specs/navbar.spec.ts
@@ -35,7 +35,7 @@ test.describe("Navbar tests", () => {
       await expect(homePage.navbar.getStartedLink).toBeHidden();
     });
 
-    test.only("logged out clicking on the menu will show navbar items #mobile", async ({
+    test("logged out clicking on the menu will show navbar items #mobile", async ({
       homePage,
     }) => {
       await homePage.navbar.mobileMenu.click();
@@ -44,10 +44,63 @@ test.describe("Navbar tests", () => {
       await expect(homePage.navbar.mobileMenuItems.getStarted).toBeVisible();
     });
 
+    test("logged out menu items will have the correct names #mobile", async ({
+      homePage,
+    }) => {
+      await homePage.navbar.mobileMenu.click();
+      await expect(homePage.navbar.mobileMenuItems.pricing).toHaveText(
+        "Pricing"
+      );
+      await expect(homePage.navbar.mobileMenuItems.signIn).toHaveText(
+        "Sign in"
+      );
+      await expect(homePage.navbar.mobileMenuItems.getStarted).toHaveText(
+        "Get started"
+      );
+    });
+
     test("logged out mobile menu is displayed on #mobile", async ({
       homePage,
     }) => {
       await expect(homePage.navbar.mobileMenu).toBeVisible();
+    });
+  });
+
+  test.describe("logged in navbar tests", () => {
+    test("logged in navbar title is Yanshuf.ai", async ({ homePage }) => {
+      await expect(homePage.navbar.navbarTitle).toContainText("Yanshuf.ai");
+    });
+
+    test("logged in displays the theme toggle #no-mobile", async ({
+      homePage,
+    }) => {
+      await expect(homePage.navbar.themeToggle).toBeVisible();
+    });
+
+    test("logged in displays the theme toggle #mobile", async ({
+      homePage,
+    }) => {
+      await expect(homePage.navbar.mobileThemeToggle).toBeVisible();
+    });
+
+    test("logged in clicking on the user menu will show navbar items #mobile", async ({
+      homePage,
+    }) => {
+      await homePage.navbar.mobileMenu.click();
+      await expect(homePage.navbar.userMenuItems.name).toHaveText("Yanshuf");
+      await expect(homePage.navbar.userMenuItems.email).toHaveText(
+        "sefiuzan812@gmail.com"
+      );
+      await expect(homePage.navbar.userMenuItems.dashboard).toBeVisible();
+      await expect(homePage.navbar.userMenuItems.settings).toBeVisible();
+      await expect(homePage.navbar.userMenuItems.subscription).toBeVisible();
+      await expect(homePage.navbar.userMenuItems.signOut).toBeVisible();
+    });
+
+    test("logged in mobile menu is displayed on #mobile", async ({
+      homePage,
+    }) => {
+      await expect(homePage.navbar.userMenu).toBeVisible();
     });
   });
 });
